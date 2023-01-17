6c94b514d95de8c329acfcf32492d326d4dcb854
taemin6697 <96530685+taemin6697@users.noreply.github.com>
2023-01-17 16:19:58 +0900

    Update ESNB.py

--- ESNB.py
+++ ESNB.py
@@ -20,12 +20,12 @@ def most(list):  #각 단어들을 카운트해서 최빈값 만들어주는거
     print(most[0])
     most2.append(most[0][0])  #[('Life', 3), ('is', 3), ('too', 4), ('short', 5)]이런식으로 나와서 인덱스처리를 단어에 맞춰서 해줌
   return most2                #만약 해당 단어가 몇번 나왔나 보려면 [0]하나 지우면 됨 근데 어차피 지금은 output 2개라 의미 없긴함
-data = pd.read_csv('C:/Users/tm011/PycharmProjects/pythonProject1/get_item_checkpoint-447300_cutout.csv')
-data0 = pd.read_csv('C:/Users/tm011/PycharmProjects/pythonProject1/get_item_checkpoint-339948_th.csv')
-data1 = pd.read_csv('C:/Users/tm011/PycharmProjects/pythonProject1/get_item_checkpoint-250488_th.csv')
-data2 = pd.read_csv("C:/Users/tm011/PycharmProjects/pythonProject1/get_item_checkpoint-250488.csv")
-data3 = pd.read_csv("C:/Users/tm011/PycharmProjects/pythonProject1/get_item_checkpoint-286272.csv")
-data4 = pd.read_csv("C:/Users/tm011/PycharmProjects/pythonProject1/get_item_checkpoint-28627288888888.csv")
+data = pd.read_csv('./four_check-point-447300.csv')
+data0 = pd.read_csv('./three_check-point-339948.csv')
+data1 = pd.read_csv('./three_check-point-250488.csv')
+data2 = pd.read_csv("./first_check-point-250488.csv")
+data3 = pd.read_csv("./first_check-point-286272.csv")
+data4 = pd.read_csv("./second_check-point-232596.csv")
 k = tolist(data)
 a = tolist(data0)  #csv파일이 2개라 그냥 똑같은거로 씀
 b = tolist(data1)
@@ -39,4 +39,4 @@ last = most(com)
 submit = pd.read_csv('C:/Users/tm011/PycharmProjects/pythonProject1/sample_submission.csv')
 submit['label'] = last
 
-submit.to_csv('./submission_esnb_sota_FINAL_____.csv', index=False)
+submit.to_csv('./submission_esnb_sota_FINAL.csv', index=False)
